2f2ba75d873cc983bc233251d814f609a7f75494
jtmcdole <john@mcdole.org>
2026-06-15 22:16:08 -0700

    0.4 termui

--- termui/flutter_bootstrap.js
+++ termui/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3676602516" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1061036295" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
